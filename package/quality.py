f64108926d29b442dcc59aabe704f3d08987b2cc
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-18 02:16:38 +0100

    Update quality.py

--- package/quality.py
+++ package/quality.py
@@ -33,7 +33,7 @@ def print_all_quality_index(q1, q2, q3, output):
     ax0.text(0.22, 0.79, 'Global quality score', fontsize = 14, fontweight='bold', transform=plt.gcf().transFigure)
     ax1 = fig.add_subplot(gs[0, 1], projection='polar')
     if q1 == -100:
-        ax1 = plot_quality_index("?", ax1, scale = 3)
+        ax1 = plot_quality_index_text("?", ax1, scale = 3)
     else:
         ax1 = plot_quality_index(round(np.mean(q1)), ax1, scale = 3)
     ax1.text(0.7, 0.88, 'Protocol quality score', fontsize = 9, fontweight='bold', transform=plt.gcf().transFigure)
@@ -78,6 +78,31 @@ def plot_quality_index(q, ax, scale):
         else:
             ax.annotate(q, xy=(1.18*np.pi, .5), color=colormap(.05+0.90*(q/max_q)), fontsize=50/scale)
   
+    return ax
+
+
+def plot_quality_index_text(q, ax, scale):
+    """Compute the quality index of the trial gait events detection (between 0 and 100) and produce a picture of the number surrounded by an appropriately colored circle. 
+
+    Parameters
+    ----------
+        q {int} -- quality index 
+        ax {} -- ax in output fig
+    """
+  
+    # plot qi
+    xval = np.arange(0, 2*np.pi*(.05+0.90), 0.01)
+    colormap = plt.get_cmap("Greys")
+    norm = mpl.colors.Normalize(0.0, 2*np.pi)
+    # f, ax = plt.subplots(nrows=1, ncols=1, figsize=(4,4),subplot_kw=dict(projection='polar'))
+    #Scatter version
+    yval = np.ones_like(xval)
+    ax.scatter(xval, yval, c=xval, s=300/(2*scale), cmap=colormap, norm=norm, linewidths=1)
+  
+    ax.set_axis_off()
+    ax.set_ylim(0,1.5)
+    ax.annotate(q, xy=( 1.25*np.pi, .3), color=colormap(.05+0.90*1), fontsize=50/scale)
+ 
     return ax
     
 
