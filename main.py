c4926c54278ec1076e9581710f4bed914dcd04b4
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-18 01:41:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,56 @@ def print_seg_detection(uturn_lim, freq):
 
     with open("uturn_lim.txt", "wt") as f:
         print(info_msg.format(**display_dict), file=f)
+
+
+def print_uturn(uturn_lim, data_lb, n, freq):
+    """Dump the phase segmentation computed from the trial. 
+
+    Parameters
+    ----------
+        uturn_lim {dict} -- dictionnary with uturn boundaries 
+    """
+    times = []
+    for i in range(len(uturn_lim)):
+        times.append(uturn_lim[0][1]-uturn_lim[0][0])
+
+    uturn_dict = {'Duration': len(data_lb)/freq, 
+                  'N_wanted': n, 
+                  'N_found': len(uturn_lim),
+                  'Percentage' : round(100*np.sum(times)/len(data_lb), 2), 
+                  'Mean U-turn duration': round(np.mean(times)/freq, 2), 
+                  'U-turn variation' : round(100*np.std(times), 2)}
+
+    display_dict = {'Title_1': "Search results:",
+                    'Duration': "Total duration (s): {Duration}".format(**uturn_dict),
+                    'Wanted': "Wanted: {N_wanted}".format(**uturn_dict),
+                    'Found': "Found: {N_found}".format(**uturn_dict),
+                    'Title_2': "Statistics:",
+                    'Percentage': "U-turn percentage (%): {Percentage}".format(**uturn_dict),
+                    'Mean': "Mean U-turn duration (s): {Mean U-turn duration}".format(**uturn_dict),
+                    'Variation': "U-turn variation (%): {}".format(**uturn_dict),
+                    }
+            
+            {'Total duration_title': "Total duration",
+                    'U-Turn start': "{U-Turn start}".format(**uturn_dict),
+                    'U-Turn start_sec': "{}".format(round(uturn_dict['U-Turn start']/freq, 2)),
+                    'U-Turn end_title': "U-turn end",
+                    'U-Turn end': "{U-Turn end}".format(**uturn_dict),
+                    'U-Turn end_sec': "{}".format(round(uturn_dict['U-Turn end']/freq, 2))}
+        
+    info_msg = """
+    {Title_1:^30}|{Title_2:^30}
+    ------------------------------+------------------------------
+    {Duration:<30}| {Percentage:<30}
+    {Wanted:<30}| {Mean:<30}
+    {Found:<30}| {Variation:<30}
+    """
+
+    # Dump information
+    os.chdir(data_WD) # Get back to the normal WD
+
+    with open("uturn_lim.txt", "wt") as f:
+        print(info_msg.format(**display_dict), file=f)
         
 
 if __name__ == "__main__":
@@ -74,7 +124,7 @@ if __name__ == "__main__":
         print_seg_detection(uturn_lim, freq)
     else:
         print("ok charge")
-        #print_uturn(uturn_lim, n, freq)
+        print_uturn(uturn_lim, data_lb, n, freq)
 
     # print("ok charge")
     sys.exit(0)
